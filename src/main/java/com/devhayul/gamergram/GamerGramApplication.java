8966eb2aecbbcb5b5490bf7d51404a3cf5350a16
hayul93 <parksy785@gmail.com>
2023-11-28 17:44:11 +0900

    Initial commit

---
+++ src/main/java/com/devhayul/gamergram/GamerGramApplication.java
@@ -0,0 +1,13 @@
+package com.devhayul.gamergram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamerGramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamerGramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devhayul/gamergram/GamerGramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devhayul.gamergram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamerGramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
